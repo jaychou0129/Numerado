b82f28a7692c8dd1b588d78bcbdc1594721dcf7a
Jay Chou <jaychou@berkeley.edu>
2023-11-25 01:58:34 +0800

    rewrite options API into composition API

--- src/components/ListeningPractice.vue
+++ src/components/ListeningPractice.vue
@@ -1,3 +1,99 @@
+<script setup>
+import "talkify-tts";
+
+import { gameSettings } from "@/scripts/gameSettings";
+import { getRandomNumber } from "@/scripts/utils";
+import KeyboardKey from "./KeyboardKey.vue";
+import { watch, computed, ref, onMounted, onBeforeUnmount } from "vue";
+
+const GameState = {
+  INITIAL: 0,
+  TIMER_STARTED: 1,
+  TIMER_STOPPED: 2,
+};
+
+const gameState = ref(GameState.INITIAL);
+const generatedNumber = ref(undefined);
+const userInput = ref("");
+const timer = ref(undefined);
+const UserInputTextbox = ref();
+const localizedNumber = computed(() => {
+  if (generatedNumber.value === undefined) return undefined;
+  return generatedNumber.value.toLocaleString(gameSettings.voice.language);
+});
+const userInputNumber = computed(() => {
+  const userInputFiltered = userInput.value.replace(/\D/g, "");
+  if (userInputFiltered == "") return undefined;
+  return parseInt(userInputFiltered);
+});
+
+const handleKeydown = (event) => {
+  switch (event.key) {
+    case "R":
+    case "r":
+      if (gameState.value === GameState.TIMER_STARTED) {
+        playGeneratedNumberAudio();
+        event.preventDefault();
+      }
+      return;
+    case " ":
+      if (
+        gameState.value === GameState.INITIAL ||
+        gameState.value === GameState.TIMER_STOPPED
+      ) {
+        // Generate new number & start the timer & play the audio
+        generatedNumber.value = getRandomNumber(
+          gameSettings.numberGeneratorMin,
+          gameSettings.numberGeneratorMax
+        );
+
+        gameState.value = GameState.TIMER_STARTED;
+        timer.value = 0.0;
+        userInput.value = "";
+
+        setTimeout(() => {
+          UserInputTextbox.value.focus();
+        });
+        playGeneratedNumberAudio();
+      }
+  }
+};
+
+const playGeneratedNumberAudio = () => {
+  window.talkifyVoicePlayer.playText(localizedNumber.value);
+};
+
+const generateVoicePlayer = () => {
+  window.talkifyVoicePlayer = new window.talkify.Html5Player()
+    .forceLanguage(gameSettings.voice.language)
+    .usePitch(gameSettings.voice.pitch)
+    .setRate(gameSettings.voice.rate);
+};
+
+watch(userInputNumber, (guessedValue) => {
+  if (guessedValue !== generatedNumber.value) return;
+
+  // Stop the timer
+  gameState.value = GameState.TIMER_STOPPED;
+});
+watch(timer, (value) => {
+  if (value === undefined) return;
+  if (gameState.value != GameState.TIMER_STARTED) return;
+  setTimeout(() => {
+    timer.value += 0.1;
+  }, 100);
+});
+watch(() => gameSettings.voice, generateVoicePlayer, { deep: true });
+
+onMounted(() => {
+  window.addEventListener("keydown", handleKeydown);
+  generateVoicePlayer();
+});
+onBeforeUnmount(() => {
+  window.removeEventListener("keydown", handleKeydown);
+});
+</script>
+
 <template>
   <v-container class="fill-height flex-column">
     <v-row>
@@ -36,121 +132,6 @@
     </v-row>
   </v-container>
 </template>
-
-<script>
-import "talkify-tts";
-
-import { gameSettings } from "@/scripts/gameSettings";
-import { getRandomNumber } from "@/scripts/utils";
-import KeyboardKey from "./KeyboardKey.vue";
-
-const GameState = {
-  INITIAL: 0,
-  TIMER_STARTED: 1,
-  TIMER_STOPPED: 2,
-};
-
-export default {
-  name: "ReadAloudPractice",
-  components: {
-    KeyboardKey,
-  },
-  data: () => ({
-    gameState: GameState.INITIAL,
-    generatedNumber: undefined,
-    userInput: "",
-    timer: undefined,
-    gameSettings,
-  }),
-  computed: {
-    localizedNumber() {
-      if (this.generatedNumber === undefined) return undefined;
-      return this.generatedNumber.toLocaleString(
-        this.gameSettings.voice.language
-      );
-    },
-    userInputNumber() {
-      const userInputFiltered = this.userInput.replace(/\D/g, "");
-      if (userInputFiltered == "") return undefined;
-      return parseInt(userInputFiltered);
-    },
-  },
-  watch: {
-    userInputNumber: {
-      handler(guessedValue) {
-        if (guessedValue !== this.generatedNumber) return;
-
-        // Stop the timer
-        this.gameState = GameState.TIMER_STOPPED;
-      },
-    },
-    timer: {
-      handler(value) {
-        if (value === undefined) return;
-        if (this.gameState != GameState.TIMER_STARTED) return;
-        setTimeout(() => {
-          this.timer += 0.1;
-        }, 100);
-      },
-    },
-    "gameSettings.voice": {
-      handler() {
-        // regenerate voice player
-        this.generateVoicePlayer();
-      },
-      deep: true,
-    },
-  },
-  mounted() {
-    window.addEventListener("keydown", this.handleKeydown);
-    this.generateVoicePlayer();
-  },
-  beforeUnmount() {
-    window.removeEventListener("keydown", this.handleKeydown);
-  },
-  methods: {
-    handleKeydown(event) {
-      switch (event.key) {
-        case "R":
-        case "r":
-          if (this.gameState === GameState.TIMER_STARTED) {
-            this.playGeneratedNumberAudio();
-            event.preventDefault();
-          }
-          return;
-        case " ":
-          if (
-            this.gameState === GameState.INITIAL ||
-            this.gameState === GameState.TIMER_STOPPED
-          ) {
-            // Generate new number & start the timer & play the audio
-            this.generatedNumber = getRandomNumber(
-              gameSettings.numberGeneratorMin,
-              gameSettings.numberGeneratorMax
-            );
-
-            this.gameState = GameState.TIMER_STARTED;
-            this.timer = 0.0;
-            this.userInput = "";
-            setTimeout(() => {
-              this.$refs.UserInputTextbox.focus();
-            });
-            this.playGeneratedNumberAudio();
-          }
-      }
-    },
-    playGeneratedNumberAudio() {
-      window.talkifyVoicePlayer.playText(this.localizedNumber);
-    },
-    generateVoicePlayer() {
-      window.talkifyVoicePlayer = new window.talkify.Html5Player()
-        .forceLanguage(this.gameSettings.voice.language)
-        .usePitch(this.gameSettings.voice.pitch)
-        .setRate(this.gameSettings.voice.rate);
-    },
-  },
-};
-</script>
 
 <style scoped>
 h1 {
