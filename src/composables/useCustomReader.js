17431882b1ce11651a85204661994007a6c2e2f5
Jay Chou <jaychou@berkeley.edu>
2023-11-28 14:17:47 +0800

    fix playAudio glitch

--- src/composables/useCustomReader.js
+++ src/composables/useCustomReader.js
@@ -1,12 +1,22 @@
 import { useSpeechSynthesis } from "@vueuse/core";
 
-export const useCustomReader = (text, voiceOptions) => {
-  const { speak, stop, isSupported } = useSpeechSynthesis(text, voiceOptions);
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-  const playAudio = () => {
+export const useCustomReader = (text, voiceOptions) => {
+  const { speak, stop, isSupported, isPlaying } = useSpeechSynthesis(
+    text,
+    voiceOptions
+  );
+
+  const playAudio = async () => {
     // Need to stop, wait a tiny bit to start again for it to work.
-    stop();
-    setTimeout(speak, 10);
+    if (isPlaying) {
+      stop();
+      await sleep(10);
+    }
+    speak();
   };
 
   return { playAudio, isTTSSupported: isSupported, stopAudio: stop };
